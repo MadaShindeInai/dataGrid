7f0ff1a0ca175b98dd6b931a51c38ac129e652d4
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-03-12 15:00:21 +0300

    feat: add margins

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import './App.scss';
-// import { FixedSizeGrid as Grid } from 'react-window';
-// import AutoSizer from "react-virtualized-auto-sizer";
 import { data } from '../../data';
-// import Row from '../Row';
-// import Controlls from '../Controlls'
 import FullTable from '../FullTable/FullTable'
-// import TableWrapper from '../TableWrapper/TableWrapper'
 
 function App() {
   return (
-    <div className="App">
-      <FullTable data={data} />
-      {/* <Controlls data={arrOfTitles} />
-      <AutoSizer>
-        {({ height, width }) => (
-          <Grid
-            className='app-grid'
-            itemData={data}
-            columnCount={7}
-            columnWidth={200}
-            height={height}
-            rowCount={1000}
-            rowHeight={35}
-            width={width}
-          >
-            {Row}
-          </Grid>
-        )}
-      </AutoSizer> */}
+    <div className="wrapper">
+      <div className="app">
+        <FullTable data={data} />
+      </div>
+
 
     </div >
   );
