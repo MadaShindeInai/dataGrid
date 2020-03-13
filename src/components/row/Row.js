899eb9882412d9adcb5b21baabc6c639b2690cf8
Sergei Nepryakhin <sergey.nepryahin@dashboquet.com>
2020-03-13 21:57:09 +0300

    feat: add chackbox

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -3,14 +3,16 @@ import './Row.scss';
 
 const PADDING_SIZE = 30;
 const Row = ({ columnIndex, rowIndex, data, style }) => {
+  console.log(typeof columnIndex, columnIndex);
 
   return (
     <div className='app-row '>
-      <span style={{
+      : <span style={{
         ...style,
         top: `${parseFloat(style.top) + PADDING_SIZE}px`
       }
-      } className="app-cell">{data[rowIndex][`${columnIndex}`]}</span>
+      } className="app-cell">{columnIndex === 7 ? <input checked={data[rowIndex][columnIndex]} id={rowIndex} type='checkbox' /> : data[rowIndex][columnIndex]}</span>
+
     </div>
   )
 }
